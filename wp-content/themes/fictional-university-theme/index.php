3456ec8366f60b56caaeda11d440f21b110ba718
Craig Eastwood <craigdavideastwood@gmail.com>
2017-11-01 17:44:03 +1100

    Posts pagination and single page posts cleanup

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -28,6 +28,7 @@
 				</div>
 		<?php
 			}
+			echo paginate_links();
 		?>
 
 	</div>
--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -2,8 +2,21 @@
 	get_header(); 
 	while (have_posts()) {
 		the_post(); ?>
-		<h2><?php the_title() ?></h2>
-		<?php the_content(); ?>
+		<div class="page-banner">
+			<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+			<div class="page-banner__content container container--narrow">
+		 		<h1 class="page-banner__title"><?php the_title(); ?></h1>
+				<div class="page-banner__intro">
+					<p>COME BACK AND FIX THIS SUB-TITLE LATER!</p>
+				</div>
+			</div>  
+		</div>
+		<div class="container container--narrow page-section">
+			<div class="metabox metabox--position-up metabox--with-home-link">
+				<p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('F j, Y') ?> in <?php echo get_the_category_list(', ');?></span></p>
+		    </div>
+			<div class="generic-content"><?php the_content(); ?></div>
+		</div>
 <?php	}
 	get_footer();
 ?>
