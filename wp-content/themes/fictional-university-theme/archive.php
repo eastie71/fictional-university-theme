a5a80fb7ea794c07eb79dbe5a575032e04cb812d
Craig Eastwood <craigdavideastwood@gmail.com>
2017-11-01 18:26:38 +1100

    Archive page for category, author and dates done

---
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -0,0 +1,48 @@
+<?php
+	get_header();
+?>
+	<div class="page-banner">
+		<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+		<div class="page-banner__content container container--narrow">
+			<h1 class="page-banner__title">
+				<?php
+					if (is_category()) {
+						single_cat_title();
+					} else if (is_author()) {
+						echo "Posts by "; the_author();
+					} else {
+						the_archive_title();
+					}
+				?>
+			</h1>
+			<div class="page-banner__intro">
+				<p><?php the_archive_description(); ?></p>
+			</div>
+		</div>  
+	</div>
+
+	<div class="container container--narrow page-section">
+		<?php
+			while (have_posts()) {
+				the_post();
+		?>
+				<div class="post-item">
+					<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<div class="metabox">
+						<p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('F j, Y') ?> in <?php echo get_the_category_list(', ');?></p>
+					</div>
+					<div class="generic-contents">
+						<?php the_excerpt(); ?>
+						<p><a class="btn btn--blue" href="<?php the_permalink()?>">Continue reading &raquo;</a></p>
+					</div>
+				</div>
+		<?php
+			}
+			echo paginate_links();
+		?>
+
+	</div>
+
+<?php
+	get_footer();
+?>
