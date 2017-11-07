cf4bb22088effaea1982b62f3e058670f34a48d2
Craig Eastwood <craigdavideastwood@gmail.com>
2017-11-07 16:41:21 +1100

    Refactor to split event summary section in content-event php file

--- wp-content/themes/fictional-university-theme/archive-event.php
+++ wp-content/themes/fictional-university-theme/archive-event.php
@@ -9,27 +9,7 @@
 		<?php
 			while (have_posts()) {
 				the_post();
-		?>
-			<div class="event-summary">
-	            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-	                <span class="event-summary__month">
-		                <?php
-		                	$eventDate = new DateTime(get_field('event_date')); 
-		                    echo $eventDate->format('M');
-		                ?>
-	                </span>
-	                <span class="event-summary__day">
-	                	<?php 
-	                    	echo $eventDate->format('d');
-	                	?>                  
-	                </span>  
-              	</a>
-	            <div class="event-summary__content">
-	                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-	                <p><?php echo wp_trim_words(get_the_content(), 30); ?><a href="<?php the_permalink(); ?>" class="nu gray"> Learn more</a></p>
-	            </div>
-            </div>
-		<?php
+				get_template_part('template-parts/content-event');
 			}
 			echo paginate_links();
 		?>
--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -82,35 +82,7 @@
           if ($homepageEvents->have_posts()) {
 	          while ($homepageEvents->have_posts()) {
 	            $homepageEvents->the_post();
-	        ?>
-	            <div class="event-summary">
-	              <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-	                <span class="event-summary__month">
-	                  <?php
-	                    $eventDate = new DateTime(get_field('event_date')); 
-	                    echo $eventDate->format('M');
-	                  ?>
-	                </span>
-	                <span class="event-summary__day">
-	                  <?php 
-	                    echo $eventDate->format('d');
-	                  ?>                  
-	                </span>  
-	              </a>
-	              <div class="event-summary__content">
-	                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-	                <p><?php
-	                      if (has_excerpt()) { 
-	                        echo get_the_excerpt();
-	                      } else {
-	                        echo wp_trim_words(get_the_content(), 18); 
-	                      }
-	                   ?>
-	                   <a href="<?php the_permalink(); ?>" class="nu gray"> Learn more</a>
-	                 </p>
-	              </div>
-	            </div>
-	        <?php
+	        		get_template_part('template-parts/content-event');
 	          } 
 	        } else {
 	        	echo "None Found.";
