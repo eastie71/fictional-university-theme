d7c97249b808f681688cd81b0e0e3bb02c98fe6a
Craig Eastwood <craigdavideastwood@gmail.com>
2018-02-07 13:22:09 +1100

    Fix footer links to main pages

--- app/wp-content/themes/fictional-university-theme/footer.php
+++ app/wp-content/themes/fictional-university-theme/footer.php
@@ -14,9 +14,9 @@
             <nav class="nav-list">
               <ul>
                 <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
+                <li><a href="<?php echo site_url('/programs'); ?>">Programs</a></li>
+                <li><a href="<?php echo site_url('/events'); ?>">Events</a></li>
+                <li><a href="<?php echo site_url('/campuses'); ?>">Campuses</a></li>
               </ul>
             </nav>
           </div>
